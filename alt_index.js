421770cd249cf996c38e460a4bb66717a0dbb7c6
Hollikill <hollister.the.great@gmail.com>
2020-01-30 21:16:53 -0600

    some styling

---
+++ alt_index.js
@@ -0,0 +1,38 @@
+var scrollpos;
+
+function getScrollingPosition()      
+{      
+  var position = [0, 0];      
+      
+  if (typeof window.pageYOffset != 'undefined')      
+  {      
+    position = [      
+        window.pageXOffset,      
+        window.pageYOffset      
+    ];      
+  }      
+      
+  else if (typeof document.documentElement.scrollTop      
+      != 'undefined' && document.documentElement.scrollTop > 0)      
+  {      
+    position = [      
+        document.documentElement.scrollLeft,      
+        document.documentElement.scrollTop      
+    ];      
+  }      
+      
+  else if (typeof document.body.scrollTop != 'undefined')      
+  {      
+    position = [      
+        document.body.scrollLeft,      
+        document.body.scrollTop      
+    ];      
+  }      
+      
+  return position;      
+}
+
+window.onscroll = function()      
+{      
+  scrollpos = getScrollingPosition();
+};
