43e8e306e05a49f68a3f5fda7349aa7e4dafca0d
Hollister Ream <hollister.the.great@gmail.com>
2020-10-27 12:17:55 -0500

    Update style.js

--- javascript/style.js
+++ javascript/style.js
@@ -35,12 +35,6 @@ function getCookie(cname) {
     }
     return "";
 }
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
 
 
 //from user https://stackoverflow.com/users/1851972/benji in post https://stackoverflow.com/questions/14292997/changing-style-sheet-javascript
