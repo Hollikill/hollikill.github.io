a6605ab1cbba24c6188792cb3b0a6e643b05cc74
Hollister Ream <hollister.the.great@gmail.com>
2021-08-23 13:59:27 -0500

    Updated Index & Typos

--- php/subfpagehead.php
+++
@@ -1,5 +0,0 @@
-<link id="pagestyle" rel="stylesheet" type="text/css" href="../css/light.css"/>
-<link rel="stylesheet" type="text/css" href="../css/style.css"/>
-<script src="../javascript/subfstyle.js"></script>
-<script src="../javascript/spoiler.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Varela+Round&display=swap" rel="stylesheet">
